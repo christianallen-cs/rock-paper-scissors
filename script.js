7ea11ba8581e1ff0a6bcffa139436c7bcd8d8607
Christian <68942126+christianallen-cs@users.noreply.github.com>
2023-04-16 22:36:07 -0400

    Added functionality to playRound

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return ("It's a tie!")
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            computerScore++;
+            return ("You lose, paper beats rock!");
+        } else {
+            playerScore++;
+            return ("You win, paper beats rock!");
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            computerSelection++;
+            return ("You lose, rock beats scissors!");
+        } else {
+            playerScore++;
+            return ("You win, rock beats scissors!")
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            playerScore++;
+            return ("You win, scissors beats paper!");
+        } else {
+            computerScore++;
+            return ("You lost, scissors beat paper!")
+        }
+    }
 }
 
 const playerSelection = "rock";
