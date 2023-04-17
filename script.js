ab2954b99d39b46586ac5cf18fafb60a46bc05ac
Christian <68942126+christianallen-cs@users.noreply.github.com>
2023-04-16 22:22:34 -0400

    Added playRound function

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function getComputerChoice() {
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
