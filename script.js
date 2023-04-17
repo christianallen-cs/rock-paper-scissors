a11b5e5362f92b82b58836287f6ed6ba34c7370d
Christian <68942126+christianallen-cs@users.noreply.github.com>
2023-04-16 21:44:54 -0400

    Added getComputerChoice

---
+++ script.js
@@ -0,0 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
